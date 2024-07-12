25f800e148173d1e593c55899de91fe7983275e1
Sofia Davis <sofidavis2107@gmail.com>
2024-07-12 16:00:29 -0400

    Initial Commit

---
+++ Day 5/Day_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_5App.swift
+//  Day 5
+//
+//  Created by Scholar on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Day_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
